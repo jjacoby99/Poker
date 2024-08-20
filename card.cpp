66c19eb0d153e6d988ca34e25fab50cc2b68fd4e
Josh <joshskisoccer@gmail.com>
2024-08-19 20:20:57 -0700

    Added get methods for suit and face value

--- card.cpp
+++ card.cpp
@@ -4,6 +4,17 @@ public:
     Card(int s, int val) : suit(s), value(val) {}
 
     Card() : suit(0), value (0) {}
+
+    int GetSuit()
+    {
+        return this->suit;
+    }
+
+    int GetValue()
+    {
+        return this->value;
+    }
+
 private:
     int suit;
     int value;
