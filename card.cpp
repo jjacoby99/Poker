0a0f67d912b0c1e0182325a7ccb9d02d641b0a54
Josh <joshskisoccer@gmail.com>
2024-08-19 20:41:52 -0700

    Updated card class with enum for suit and fv

--- card.cpp
+++ card.cpp
@@ -3,9 +3,36 @@
 class Card
 {
 public: 
-    Card(int s, int val) : suit(s), value(val) {}
+    // Enum for Suit
+    enum class Suit {
+        CLUBS = 1,
+        DIAMONDS = 2,
+        HEARTS = 3,
+        SPADES = 4
+    };
 
-    Card() : suit(0), value (0) {}
+    // Enum for FaceValue
+    enum class FaceValue {
+        ACE = 1,
+        TWO = 2,
+        THREE = 3,
+        FOUR = 4,
+        FIVE = 5,
+        SIX = 6,
+        SEVEN = 7,
+        EIGHT = 8,
+        NINE = 9,
+        TEN = 10,
+        JACK = 11,
+        QUEEN = 12,
+        KING = 13
+    };
+
+    // constructor 
+    Card(Suit s, FaceValue val) : suit(s), value(val) {}
+
+    // default constructor: Ace of Spades
+    Card() : suit(Suit::SPADES), value(FaceValue::ACE) {}
 
     int GetSuit()
     {
@@ -22,15 +49,15 @@ public:
         std::string res;
         
         // determine face value
-        res += this->faceSymbols[this->value];
+        res += this->faceSymbols[static_cast<int>(this->value)];
 
         // determine suit
-        res += this->suitSymbols[this->suit];
+        res += this->suitSymbols[static_cast<int>(this->suit)];
     }
 
 private:
-    int suit;
-    int value;
+    Suit suit;
+    FaceValue value;
     
     // Array to map face values to symbols
     const char* faceSymbols[13] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"};
