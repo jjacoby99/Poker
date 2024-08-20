a6423a30758f0087279dea98ddf0c2a738681ead
Josh <joshskisoccer@gmail.com>
2024-08-19 20:34:33 -0700

    Added ToString method

--- card.cpp
+++ card.cpp
@@ -1,3 +1,5 @@
+#include <string>
+ 
 class Card
 {
 public: 
@@ -15,7 +17,24 @@ public:
         return this->value;
     }
 
+    std::string ToString()
+    {
+        std::string res;
+        
+        // determine face value
+        res += this->faceSymbols[this->value];
+
+        // determine suit
+        res += this->suitSymbols[this->suit];
+    }
+
 private:
     int suit;
     int value;
+    
+    // Array to map face values to symbols
+    const char* faceSymbols[13] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"};
+
+    // Array to map suit values to symbols
+    const char* suitSymbols[4] = {"s", "h", "d", "c"};
 };
