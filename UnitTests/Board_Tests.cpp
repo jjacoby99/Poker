368682c8b5e27d95ed6033aa2d73dd8b21b95f7c
Josh <joshskisoccer@gmail.com>
2024-08-22 10:47:52 -0700

    Added unit tests for Board

--- UnitTests/Board_Tests.cpp
+++ UnitTests/Board_Tests.cpp
@@ -21,3 +21,43 @@ TEST(BoardTest, FlopConstructor)
     EXPECT_EQ(deckstr.substr(0,6), actual);
 
 }
+
+TEST(BoardTest, TurnConstructor)
+{
+    // create default deck
+    Deck deck;
+
+    std::string deckstr = deck.ToString();
+
+    Board board(deck.Deal(3), deck.Deal(1)[0]);
+
+    std::vector<Card> flop = board.GetBoard();
+
+    std::string actual;
+    for(Card c: flop)
+    {
+        actual += c.ToString();
+    }
+
+    EXPECT_EQ(deckstr.substr(0,8), actual);
+}
+
+TEST(BoardTest, RiverConstructor)
+{
+    // create default deck
+    Deck deck;
+
+    std::string deckstr = deck.ToString();
+
+    Board board(deck.Deal(3), deck.Deal(1)[0], deck.Deal(1)[0]);
+
+    std::vector<Card> flop = board.GetBoard();
+
+    std::string actual;
+    for(Card c: flop)
+    {
+        actual += c.ToString();
+    }
+
+    EXPECT_EQ(deckstr.substr(0,10), actual);
+}
