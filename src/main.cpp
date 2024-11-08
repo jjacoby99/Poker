f1a6043b85662b989a32228e0bf119492a76d8a8
Josh <joshskisoccer@gmail.com>
2024-11-07 22:41:38 -0800

    Updates to main to reflect player changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include "/Users/joshjacoby/Desktop/Code/Poker/Poker/include/sim.h"
-#include "/Users/joshjacoby/Desktop/Code/Poker/Poker/include/player.h"
+//#include "/Users/joshjacoby/Desktop/Code/Poker/Poker/include/player.h"
 #include "/Users/joshjacoby/Desktop/Code/Poker/Poker/include/game.h"
-
+#include "/Users/joshjacoby/Desktop/Code/Poker/Poker/include/HumanPlayer.h"
 
 int main() {
     int players = 2;
     int bb = 2;
     int sb = 1;
 
-    Player hero(200.0, "Hero");
-    Player villain(200.0, "Villain");
+    HumanPlayer hero(200.0, "Hero");
+    HumanPlayer villain(200.0, "Villain");
 
-    Game game({hero, villain}, sb, bb);
+    Game game({hero,villain}, {}, sb, bb);
 
     game.Play();
 
