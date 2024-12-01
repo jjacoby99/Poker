74c2268528c034b9e39ef4f0d4182c371547d0a3
Josh <joshskisoccer@gmail.com>
2024-12-01 13:22:11 -0800

    Card

--- src/card.cpp
+++ src/card.cpp
@@ -3,7 +3,6 @@
 const char* Card::faceSymbols[13] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"};
 const char* Card::suitSymbols[4] = {"c", "d", "h", "s"}; // Corresponding to Clubs, Diamonds, Hearts, Spades
 
-// Constructor implementations
 Card::Card(Suit s, FaceValue val) : suit(s), value(val) {}
 
 Card::Card() : suit(Suit::SPADES), value(FaceValue::ACE) {}
